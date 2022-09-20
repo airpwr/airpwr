95d147a417ce9c5c9cf11ed556dee619c96f19ee
Thomas Bateson <thomas.bateson@eagles.oc.edu>
2022-09-20 12:19:44 -0500

    Fix install script

--- src/install.ps1
+++ src/install.ps1
@@ -1,34 +1,33 @@
-param (
-	[Parameter(Position = 0)]
-	[ValidatePattern('^[0-9]+\.[0-9]+(\.[0-9]+)?$')]
-	[string]$Version
-)
-
 $ProgressPreference = 'SilentlyContinue'
 
 # Required for curl.exe
 if ([Environment]::OSVersion.Version.Build -lt 17063) {
 	Write-Error "windows build version $([Environment]::OSVersion.Version.Build) does not meet minimum required build version 17063; update windows to use pwr"
-	exit 1
+} elseif ($env:PwrLoadedPackages -or $env:PwrShellPackages) {
+	Write-Error "already running pwr with packages $(if ($env:PwrLoadedPackages) { $env:PwrLoadedPackages } else { $env:PwrShellPackages })"
 }
 
-if (-not $Version) {
+$script:PwrVersion = $env:PwrVersion
+if (-not $script:PwrVersion) {
 	$Tags = & "$env:SYSTEMROOT\System32\curl.exe" -s --url 'https://api.github.com/repos/airpwr/airpwr/tags' | ConvertFrom-Json
 	if ($global:LASTEXITCODE) {
 		Write-Error 'could not get pwr tags'
-		exit 1
+		throw
 	}
-	$Version = $Tags[0].Name.Substring(1)
+	$script:PwrVersion = $Tags[0].Name.Substring(1)
 }
 
 $PwrPath = if ($env:PwrHome) { $env:PwrHome } else { "$env:AppData\pwr" }
 $PwrCmd = "$PwrPath\cmd"
 $PwrScriptPath = "$PwrCmd\pwr.ps1"
 mkdir $PwrCmd -Force | Out-Null
-& "$env:SYSTEMROOT\System32\curl.exe" -s --url "https://raw.githubusercontent.com/airpwr/airpwr/v$Version/src/pwr.ps1" --output $PwrScriptPath
+if ($script:PwrVersion -notmatch '^[0-9]+\.[0-9]+(\.[0-9]+)?$') {
+	Write-Error 'version does not match the expected pattern'
+}
+& "$env:SYSTEMROOT\System32\curl.exe" -s --url "https://raw.githubusercontent.com/airpwr/airpwr/v$script:PwrVersion/src/pwr.ps1" --output $PwrScriptPath
 if ($global:LASTEXITCODE) {
 	Write-Error 'could not to download pwr script'
-	exit 1
+	throw
 }
 $UserPath = [Environment]::GetEnvironmentVariable('Path', 'User')
 if (-not $UserPath.Contains($PwrCmd)) {
@@ -41,12 +40,10 @@ try {
 	& $PwrScriptPath | Out-Null
 	if ($global:LASTEXITCODE) {
 		Write-Error "pwr script finished with non-zero exit value $global:LASTEXITCODE"
-		exit 1
 	}
 } catch {
 	Write-Error $_
 	Write-Error "could not run pwr script; make sure it was installed correctly at $PwrScriptPath"
-	exit 1
 }
 
 Write-Host "pwr: " -ForegroundColor Blue -NoNewline
